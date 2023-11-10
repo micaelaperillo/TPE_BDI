26f8ae3d20c9c17e473dddced826d32843a17ead
albabjorkman <141673187+albabjorkman@users.noreply.github.com>
2023-11-10 17:15:27 -0300

    Update funciones.sql

--- funciones.sql
+++ funciones.sql
@@ -41,3 +41,52 @@ create trigger empttrigger after insert or delete or update on empleado
 for each row
 execute function empregister();
 
+CREATE OR REPLACE FUNCTION historial_empleados(pdate timestamp) RETURNS VOID AS $$
+        DECLARE 
+                myCursor CURSOR FOR SELECT legajo, nombre, sueldo, edad, tt_izq, tt_der 
+                FROM empleados_tt order by legajo, Nro_Movimiento;
+
+          rec RECORD;    
+          Nro_Movimiento int := 0;
+         
+
+BEGIN
+        RAISE NOTICE '--------------------------------------------------------------------------------';
+        RAISE NOTICE '---------------------------HISTORIAL DE EMPLEADOS-------------------------------'; 
+        RAISE NOTICE '--------------------------------------------------------------------------------';
+        RAISE NOTICE '--------Estado-------Legajo-------Sueldo-------Edad-------Nro_Movimiento--------';
+        RAISE NOTICE '--------------------------------------------------------------------------------';
+        OPEN MYCURSOR;
+LOOP
+                FETCH MYCURSOR INTO REC;
+                EXIT WHEN NOT FOUND;
+                RAISE NOTICE '';
+              PERFORM checkState(pdate, REC, Nro_Movimiento);
+                  Nro_Movimiento := Nro_Movimiento + 1;
+                END LOOP;
+
+ CLOSE myCursor;
+ end;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION checkState(ppdate timestamp, rec RECORD, Nro_Movimiento int) RETURNS VOID AS $$
+        DECLARE 
+        estado VARCHAR(30);
+   
+BEGIN
+        if Extract(year from ppdate)!=extract(YEAR FROM CURRENT_DATE) THEN
+                RAISE EXCEPTION 'la fecha tiene que ser de este año calendario';
+        ELSIF ppdate > rec.tt_izq AND rec.tt_der = 'infinity' THEN 
+        estado := 'Vigente Anterior';
+    ELSIF ppdate > rec.tt_izq AND ppdate < rec.tt_der THEN 
+        estado := 'Vigente';
+    ELSE
+        estado := 'No Vigente';
+    END IF;
+ 
+    RAISE NOTICE '% % % % %', estado, rec.legajo, rec.sueldo, rec.edad, Nro_Movimiento;
+    
+END;
+$$ LANGUAGE plpgsql;
+
+
