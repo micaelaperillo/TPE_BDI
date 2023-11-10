5c440547431d85516ab135f3f4b8b837b481d76c
albabjorkman <141673187+albabjorkman@users.noreply.github.com>
2023-11-10 15:34:08 -0300

    Update funciones.sql

--- funciones.sql
+++ funciones.sql
@@ -15,8 +15,7 @@ sueldo int,
 edad int,
 tt_izq timestamp, 
 tt_der timestamp,
-primary key (legajo),
-primary key (tt_izq),
+primary key (legajo, tt_izq),
 foreign key (legajo) references empleado,
 CHECK (tt_der>=tt_izq AND sueldo>=0 AND edad>=0));
 
