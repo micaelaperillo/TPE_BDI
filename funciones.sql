3153ed45956dd1eab7ffd7a45aa514301a85ac32
Micaela Perillo <micaelaperillo26@gmail.com>
2023-11-15 12:33:55 -0300

    Better style in funciones.sql

--- funciones.sql
+++ funciones.sql
@@ -1,23 +1,28 @@
 DROP TABLE IF EXISTS empleado;
 DROP TABLE IF EXISTS empleado_tt;
 
+------ TABLAS ------
+
 CREATE TABLE empleado (
-legajo int not null,
-nombre varchar(30),
-sueldo int,
-edad int,
-primary key (legajo));
+    legajo int not null,
+    nombre varchar(30),
+    sueldo int,
+    edad int,
+    primary key (legajo)
+);
 
 CREATE TABLE empleado_tt (
-legajo int not null,
-nombre varchar(30),
-sueldo int,
-edad int,
-tt_izq timestamp, 
-tt_der timestamp,
-primary key (legajo, tt_izq),
-CHECK (tt_der>=tt_izq AND sueldo>=0 AND edad>=0));
+    legajo int not null,
+    nombre varchar(30),
+    sueldo int,
+    edad int,
+    tt_izq timestamp, 
+    tt_der timestamp,
+    primary key (legajo, tt_izq),
+    CHECK (tt_der>=tt_izq AND sueldo>=0 AND edad>=0)
+);
 
+------ TRIGGERS ------
 
 CREATE OR REPLACE FUNCTION empregister() RETURNS TRIGGER AS $$
 BEGIN 
@@ -36,66 +41,59 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-create trigger empttrigger after insert or delete or update on empleado
-for each row
-execute function empregister();
+CREATE TRIGGER empttrigger AFTER INSERT OR DELETE OR UPDATE ON empleado
+FOR EACH ROW
+EXECUTE FUNCTION empregister();
+
+------ Historial de Empleados ------
 
 CREATE OR REPLACE FUNCTION historial_empleados(pdate timestamp) RETURNS VOID AS $$
-        DECLARE 
-                myCursor CURSOR FOR SELECT legajo, nombre, sueldo, edad, tt_izq, tt_der 
-                FROM empleado_tt order by legajo;
+    DECLARE 
+            myCursor CURSOR FOR SELECT legajo, nombre, sueldo, edad, tt_izq, tt_der 
+            FROM empleado_tt order by legajo;
 
-          rec RECORD;    
-          Nro_Movimiento int := 0;
-          leg_anterior int :=null;
+        rec RECORD;    
+        Nro_Movimiento int := 0;
+        leg_anterior int :=null;
 
-BEGIN
-           if Extract(year from pdate)!=extract(YEAR FROM CURRENT_DATE) THEN
-                RAISE notice 'La fecha tiene que ser de este año calendario';
-                return;
-           end if;
+    BEGIN
+        IF EXTRACT(year from pdate)!= EXTRACT(YEAR FROM CURRENT_DATE) THEN
+            RAISE NOTICE 'La fecha tiene que ser de este año calendario';
+            RETURN;
+        END IF;
         RAISE NOTICE '--------------------------------------------------------------------------------';
         RAISE NOTICE '---------------------------HISTORIAL DE EMPLEADOS-------------------------------'; 
         RAISE NOTICE '--------------------------------------------------------------------------------';
         RAISE NOTICE '--------Estado-------Legajo-------Sueldo-------Edad-------Nro_Movimiento--------';
         RAISE NOTICE '--------------------------------------------------------------------------------';
         OPEN MYCURSOR;
-LOOP
-                FETCH MYCURSOR INTO REC;
-
-                EXIT WHEN NOT FOUND;    
-              if(leg_anterior is null or leg_anterior=rec.legajo) then
-                  Nro_Movimiento := Nro_Movimiento + 1;
-              else 
-                  Nro_Movimiento:=1;
-             end if;
-             leg_anterior:=rec.legajo;
-             PERFORM checkState(pdate, REC, Nro_Movimiento);
- END LOOP;
-
- CLOSE myCursor;
- end;
+        LOOP
+            FETCH MYCURSOR INTO REC;
+            EXIT WHEN NOT FOUND;    
+            IF (leg_anterior IS NULL OR leg_anterior = rec.legajo) THEN
+                Nro_Movimiento := Nro_Movimiento + 1;
+            ELSE 
+                Nro_Movimiento := 1;
+            END IF;
+            leg_anterior := rec.legajo;
+            PERFORM checkState(pdate, REC, Nro_Movimiento);
+        END LOOP;
+        CLOSE myCursor;
+    END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION checkState(ppdate timestamp, rec RECORD, Nro_Movimiento int) RETURNS VOID AS $$
-        DECLARE 
+CREATE OR REPLACE FUNCTION checkState(ppdate TIMESTAMP, rec RECORD, Nro_Movimiento INT) RETURNS VOID AS $$
+    DECLARE 
         estado VARCHAR(30);
    
-BEGIN
-
-        IF ppdate > rec.tt_izq AND rec.tt_der = 'infinity' THEN 
-        estado := 'Vigente Anterior';
-    ELSIF ppdate <= rec.tt_izq AND  rec.tt_der='infinity' THEN 
-        estado := 'Vigente';
-    ELSE
-        estado := 'No Vigente';
-    END IF;
- 
-    RAISE NOTICE '% % % % %', estado, rec.legajo, rec.sueldo, rec.edad, Nro_Movimiento;
-    
-END;
+    BEGIN
+        IF (ppdate > rec.tt_izq AND rec.tt_der = 'infinity') THEN 
+            estado := 'Vigente Anterior';
+        ELSIF (ppdate <= rec.tt_izq AND  rec.tt_der='infinity') THEN 
+                estado := 'Vigente';
+        ELSE
+                estado := 'No Vigente';
+        END IF;
+        RAISE NOTICE '% % % % %', estado, rec.legajo, rec.sueldo, rec.edad, Nro_Movimiento;
+    END;
 $$ LANGUAGE plpgsql;
-
-
-
-
